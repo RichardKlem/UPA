56d6f8fa5a16cd49cd89b73d4c1b9a357bb9416d
Richard Klem <48335467+RichardKlem@users.noreply.github.com>
2021-10-24 22:42:09 +0200

    Refactor to multiplatform functions.

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -1,57 +1,61 @@
-import requests, os, re, sys, json, csv
+import csv
+import json
+import os
+import re
+from pathlib import Path
+
+import requests
 from bs4 import BeautifulSoup
 
 
 class DataDownloader:
     def __init__(self, url="https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/",
-                       folder = "data") -> None:
+                 folder="data") -> None:
         self.url = url
-        self.folder = folder
+        self.folder = Path(folder)
         pass
-
 
     """
     Module: download
     Function: downloads csv files with data from the page
     """
-    def download(self):
-        headers = {'User-Agent': 'Mozilla/5.0',}
 
-        #checks if folder data exists
-        if not os.path.isdir(self.folder):
-            os.mkdir(self.folder)
+    def download(self, number_of_files: int = None):
+        headers = {'User-Agent': 'Mozilla/5.0', }
 
-        # gets HTML form of a page
-        r = requests.get(self.url, headers = headers, allow_redirects = True)
+        os.makedirs(self.folder, exist_ok=True)
+
+        # Gets HTML form of a page.
+        r = requests.get(self.url, headers=headers, allow_redirects=True)
         soup = BeautifulSoup(r.content, "html.parser")
 
-        # gets names of csv files from page
+        # Gets names of csv files from page.
         CsvFiles = []
         for file in re.findall(r'"url":"[1-9a-z-]*.csv', str(soup)):
             CsvFiles.append(file[7:])
 
-        cnt = 0
+        cnt = 1
 
-        # saves csv file into folder data
+        # Saves csv file into data folder.
         for csvFileName in CsvFiles:
-            csvFilePath = self.folder + '/' + csvFileName
-            jsonFilePath = csvFilePath.split(".csv")[0] + ".json"
+            if number_of_files and cnt > number_of_files:
+                break
 
-            sys.stdout.write(f"\rJSON files created: {cnt}/{len(CsvFiles)}")
-            sys.stdout.flush()
+            csvFilePath = Path(os.path.join(self.folder, csvFileName))
+            jsonFilePath = csvFilePath.with_suffix('.json')
+
+            print(f"\rJSON files created: {cnt}/{len(CsvFiles)}", end='', flush=True)
             cnt += 1
 
-            # do not download or tranform the json file if it already exists
+            # Do not download or convert the json file if it already exists.
             if not os.path.isfile(jsonFilePath):
-                # do not download the csv file if it already exists
+                # Do not download the csv file if it already exists.
                 if not os.path.isfile(csvFilePath):
-                    # downloads a total of 4.5 GB of data,
-                    # so there is an interruption to test and prevent all data downloads.
-                    # Maybe we can download one file, insert the data into DB and after that move on to the next file and delet the previous one
                     r = requests.get(self.url + csvFileName)
-                    open(csvFilePath, 'wb').write(r.content)
+                    f = os.open(csvFilePath, os.O_RDWR | os.O_CREAT)
+                    os.write(f, r.content)
 
-                # fast transforming, but RAM expensive
+                # Fast transforming, but RAM expensive.
                 """
                 # transform csv file to json
                 data = pandas.DataFrame()
@@ -64,7 +68,7 @@ class DataDownloader:
                     data.to_json(jsonFilePath, orient = "records", indent=4)
                 """
 
-                # slow transforming, but does not run out of RAM (max RAM usage around 6GB)
+                # Slow transforming, but does not run out of RAM (max RAM usage around 6GB).
                 with open(csvFilePath, 'r', encoding='utf-8-sig') as csvFile:
                     reader = csv.DictReader(csvFile)
 
@@ -74,12 +78,9 @@ class DataDownloader:
                             json.dump(row, jsonFile, indent=2)
                             jsonFile.write(',\n')
 
-                        # remove trailing comma and EOL
+                        # Remove trailing comma and EOL.
                         jsonFile.truncate(jsonFile.tell() - 2)
                         jsonFile.write("]")
-                # os.remove(csvFilePath)
-
-        sys.stdout.write(f"\rJSON files created: {cnt}/{len(CsvFiles)}\n")
 
 
 if __name__ == "__main__":
