7b815cdb2561557c675931a593aab6c0fe43aad1
TomasBeranek <xberan46@stud.fit.vutbr.cz>
2021-12-08 01:55:04 +0100

    Add a visualization of question A1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import src.loader
 from data_files import data_files
 from src.data_handler import DataHandler
+from src.data_visualizer import DataVisualizer
 
 if __name__ == "__main__":
     data_folder = 'data'
@@ -12,6 +13,7 @@ if __name__ == "__main__":
 
     skip_download = False
     skip_insert = False
+    skip_visualize = False
 
     if not skip_download:
         Handler = DataHandler(data_folder=data_folder, list_of_files=selected_files)
@@ -34,3 +36,11 @@ if __name__ == "__main__":
                     if not isinstance(file_data, list):
                         file_data = [file_data]
                     src.loader.load_data(collection_name=file_name, data=file_data)
+
+    if not skip_visualize:
+        visualizer = DataVisualizer()
+        visualizer.visualizeA1( "graphs/A1.png",
+                                "data-part2/A1-hospitalizovani-example.csv",
+                                "data-part2/A1-nakazeni-example.csv",
+                                "data-part2/A1-testy-example.csv",
+                                "data-part2/A1-vyleceni-example.csv")
---
+++ src/data_visualizer.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from data_files import data_files
+import matplotlib.pyplot as plt
+
+
+class DataVisualizer:
+    """ The class uses data in CSV format and creates a set of graphs which
+        describe the data. """
+
+    def __init__(self):
+        pass
+
+    def visualizeA1(self, output_path, hospitalized_path, infected_path, tests_path, cured_path):
+        hospitalized = pd.read_csv(hospitalized_path)
+        infected = pd.read_csv(infected_path)
+        tests = pd.read_csv(tests_path)
+        cured = pd.read_csv(cured_path)
+
+        # remove day from date
+        for i in hospitalized.index:
+            hospitalized.at[i, 'datum'] = hospitalized.at[i, 'datum'][:-3]
+
+        for i in infected.index:
+            infected.at[i, 'datum'] = infected.at[i, 'datum'][:-3]
+
+        for i in tests.index:
+            tests.at[i, 'datum'] = tests.at[i, 'datum'][:-3]
+
+        for i in cured.index:
+            cured.at[i, 'datum'] = cured.at[i, 'datum'][:-3]
+
+        # sum value in month
+        hospitalized = hospitalized.groupby('datum', as_index=False).sum()
+        infected = infected.groupby('datum', as_index=False).sum()
+        tests = tests.groupby('datum', as_index=False).sum()
+        cured = cured.groupby('datum', as_index=False).sum()
+
+        # add a line for each dataset
+        plt.plot(hospitalized["datum"], hospitalized["pocet"], label = "Počet hospitalizovaných")
+        plt.plot(infected["datum"], infected["pocet"], label = "Počet nakažených")
+        plt.plot(tests["datum"], tests["pocet"], label = "Počet provedených testů")
+        plt.plot(cured["datum"], cured["pocet"], label = "Počet vyléčených")
+
+        plt.xlabel("Datum")
+        plt.ylabel("Počet")
+        plt.title("Dotaz A1")
+        plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left")
+
+        plt.savefig(output_path, bbox_inches="tight")
